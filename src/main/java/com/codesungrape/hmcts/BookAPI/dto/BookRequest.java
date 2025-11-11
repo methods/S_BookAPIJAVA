e4da445a627320781d4a1fb5ca8833a7692a80e9
codesungrape <miss.s.rai@outlook.com>
2025-11-11 19:56:01 +0000

    Clean up redundant comment

--- src/main/java/com/codesungrape/hmcts/BookAPI/dto/BookRequest.java
+++ src/main/java/com/codesungrape/hmcts/BookAPI/dto/BookRequest.java
@@ -13,7 +13,7 @@ import lombok.Value;
  */
 @Value
 public class BookRequest {
-    @NotBlank(message= "Title is required") // enforces
+    @NotBlank(message= "Title is required")
     @JsonProperty("title")
     String title;
 
