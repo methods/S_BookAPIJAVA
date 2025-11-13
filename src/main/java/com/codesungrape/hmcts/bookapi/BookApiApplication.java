4f9a0168d7f9771ce94726086442c640ae22ff53
codesungrape <miss.s.rai@outlook.com>
2025-11-13 15:21:54 +0000

    Conform to HMCTS Checkstyle; add private constructor

--- src/main/java/com/codesungrape/hmcts/bookapi/BookApiApplication.java
+++ src/main/java/com/codesungrape/hmcts/bookapi/BookApiApplication.java
@@ -3,18 +3,26 @@ package com.codesungrape.hmcts.bookapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 /**
- * Entry point for the Book API Spring Boot application.
- * Starts the Spring ApplicationContext and embedded server.
+ * Entry point for the Book API Spring Boot application. Starts the Spring ApplicationContext and
+ * embedded server.
  */
 @SpringBootApplication
 public class BookApiApplication {
-  /**
-   * * Main entry point — starts the Spring Boot application.
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(BookApiApplication.class, args);
-  }
 
+    /**
+     * Placeholder.
+     */
+    private BookApiApplication() {
+        // prevents instantiation
+    }
+
+    /**
+     * Main entry point — starts the Spring Boot application.
+     */
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookApiApplication.class, args);
+
+    }
 }
